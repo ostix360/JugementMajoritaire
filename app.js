03fc03820d8843d153b13728a5ac2efcf8b86ccc
ostix360 <vicrobin1001@gmail.com>
2022-10-10 19:52:46 +0200

    Site done

--- app.js
+++ app.js
@@ -14,15 +14,13 @@ app.use(express.json({limit : '10mb'}));
 const db = new Datastore('database.db');
 db.loadDatabase();
 
-
+const axiosClient = axios.create({baseURL: 'https://discord.com', timeout: 20000});
 
 app.post('/send', async (req,res) => {
     console.log("approve");
-	console.log(req.body);
+	let data = {content:JSON.stringify(req.body)};
 	
-	let data = req.body;
-	
-	axios.post('https://discord.com/api/webhooks/1029077387964588172/6lqA-Q4UY4vqhg0gObtCcJuCzxXtIIwxaNS3e9AwReJxMA5FBRMr67vBzvXKcfmFymKt', data=data);
+	axiosClient.post('/api/webhooks/1029077387964588172/6lqA-Q4UY4vqhg0gObtCcJuCzxXtIIwxaNS3e9AwReJxMA5FBRMr67vBzvXKcfmFymKt', data=data);
 	db.insert(req.body);
 	res.status(200);
 });
